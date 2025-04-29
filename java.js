3972eff3ceee39010dfcee036fc2f78a41e3e2dd
AnaGZPortfolio <agiraldo12@uan.edu.co>
2025-04-29 11:32:36 -0500

    java.js

--- java.js
+++ java.js
@@ -1,1 +1,19 @@
-// JavaScript source code
+ <script>
+        const imagenes = [
+            'imagen1',
+            'imagen2',
+            'imagen3',
+        ];
+        let indice = 0;
+
+        function cambiarImagen(direccion) {
+            indice += direccion;
+            if (indice < 0) {
+                indice = imagenes.length - 1;
+            } else if (indice >= imagenes.length) {
+                indice = 0;
+            }
+
+            document.getElementById('imagen').src =imagenes;
+        }
+    </script> JavaScript source code
