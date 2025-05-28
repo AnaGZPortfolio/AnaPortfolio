37e91a29b2b5f7ac3a960e1bbed70ce30a31564f
AnaGZPortfolio <agiraldo12@uan.edu.co>
2025-05-28 07:00:31 -0500

    java.js

--- java.js
+++ java.js
@@ -1,19 +1,3 @@
- <script>
-        const imagenes = [
-            'imagen1',
-            'imagen2',
-            'imagen3',
-        ];
-        let indice = 0;
-
-        function cambiarImagen(direccion) {
-            indice += direccion;
-            if (indice < 0) {
-                indice = imagenes.length - 1;
-            } else if (indice >= imagenes.length) {
-                indice = 0;
-            }
-
-            document.getElementById('imagen').src =imagenes;
-        }
+ <script src>
+       
     </script> JavaScript source code
